8cce41b13117200182c43ebfcaed5268617742ca
vaatanvastuhallivolvi <70329942+vaatanvastuhallivolvi@users.noreply.github.com>
2020-08-27 16:56:46 +0300

    Add files via upload

---
+++ tort.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+a = int(input("Mitu küpsist mahub kandikule laiuses: "))
+b = int(input("Mitu küpsist mahub kandikule pikkuses: "))
+c = int(input("Mitu kihti on tordil: "))
+d = int(input("Mitu küpsist on ühes pakis: "))
+
+pakkide_arv =  ceil(a * b * c / d)
+lause_osa = " küpsisepakki tuleb tordi jaoks osta."
+lause = str(pakkide_arv)+lause_osa
+
+print(lause)
