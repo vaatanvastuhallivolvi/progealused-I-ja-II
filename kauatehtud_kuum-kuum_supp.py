86ca546673e50a2d41f3c522e71df60751989e1d
vaatanvastuhallivolvi <70329942+vaatanvastuhallivolvi@users.noreply.github.com>
2020-08-27 17:15:04 +0300

    Update kauatehtud_kuum-kuum_supp.py

--- kauatehtud_kuum-kuum_supp.py
+++ kauatehtud_kuum-kuum_supp.py
@@ -1,11 +1,11 @@
-a = int(input("Kui kõrge on supi algtemperatuur: "))
-t = int(input("Kui kõrge on toatemperatuur: "))
+algus = int(input("Kui kõrge on supi algtemperatuur: "))
+tuba = int(input("Kui kõrge on toatemperatuur: "))
 
-k = 0.19
-jahtuv_supp = a
+kordaja = 0.19
+jahtuv_supp = algus
 minut = 0
 while minut < 10:
-    jahtuv_supp = jahtuv_supp - (jahtuv_supp - t) * k
+    jahtuv_supp = jahtuv_supp - (jahtuv_supp - tuba) * kordaja
     minut = minut + 1
 
 print("Supi temperatuur " + str(minut) + " minuti pärast on: " + str(round(jahtuv_supp)) + " kraadi" + ".")
