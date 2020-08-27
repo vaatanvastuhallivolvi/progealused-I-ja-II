917b40b76a78a6136136047f9e40a96049391b12
vaatanvastuhallivolvi <70329942+vaatanvastuhallivolvi@users.noreply.github.com>
2020-08-27 17:11:37 +0300

    Update sendiboss.py

--- sendiboss.py
+++ sendiboss.py
@@ -1,24 +1,24 @@
-print("Kui soovite avaldada, siis palju on Teil eurosente: ")
+print("Palun öelge, kui palju on Teil eurosente: ")
 sendid = int(input())
 
-mündiäss = sendid // 100
+eurokas = sendid // 100
 jääk = sendid % 100
 
 if sendid == 100:
     print("1 euro")
 elif sendid > 1 and jääk == 0:
-    print(str(mündiäss) + " eurot ")
+    print(str(eurokas) + " eurot ")
 elif sendid == 1:
     print("1 sent")
 elif sendid < 100 and sendid > 1:
     print(str(sendid) + " senti")
 elif sendid < 199 and sendid > 99 and jääk < 2:
-    print(str(mündiäss) + " euro ja " + "1 sent")
+    print(str(eurokas) + " euro ja " + "1 sent")
 elif sendid > 199 and jääk < 2:
-    print(str(mündiäss) + " eurot ja " + "1 sent")
+    print(str(eurokas) + " eurot ja " + "1 sent")
 elif sendid > 199 and jääk > 1:
-    print(str(mündiäss) + " eurot ja " + str(jääk) + " senti")
+    print(str(eurokas) + " eurot ja " + str(jääk) + " senti")
 elif sendid < 200 and sendid > 99:
-    print(str(mündiäss) + " euro ja " + str(jääk) + " senti")
+    print(str(eurokas) + " euro ja " + str(jääk) + " senti")
 else:
-    print("Oled rahatu õilishing.")
+    print("Kauem tulevik on müntideta küttide ja korilaste päralt.")
