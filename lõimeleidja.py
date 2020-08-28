881a3976b162515ea52a5d739c1289084a51d805
vaatanvastuhallivolvi <70329942+vaatanvastuhallivolvi@users.noreply.github.com>
2020-08-28 16:10:52 +0300

    Add files via upload

---
+++ lõimeleidja.py
@@ -0,0 +1,25 @@
+def lõimede_pikkus(vaip, lõimed):
+    return round(lõimed * (float(vaip) * 1.2 + 0.5), 2)
+
+lendav = input('Palun sisestage otsitava faili pealkiri "Lendavad_vaibad.txt": ')
+pikemad = int(input("Palun sisestage kõrvuti olevate lõimede arv 5-meetriste ja pikemate vaipade puhul: "))
+lühemad = int(input("Palun sisestage kõrvuti olevate lõimede arv vaipade puhul, mis on lühemad kui 5 meetrit: "))
+
+with open(lendav, encoding = "UTF-8") as fail: 
+    vaibad = fail.read().splitlines()
+
+lõimed2 = []
+for el in vaibad:
+    if float(el) >= 5:
+        realiteet = lõimede_pikkus(el, pikemad)
+    else:
+        realiteet = lõimede_pikkus(el, lühemad)
+    print(realiteet)
+    lõimed2 += [realiteet]
+    
+summa = 0
+for realiteet in lõimed2:
+    summa += realiteet
+
+summa = round(summa, 2)
+print("Kõigi vaipade peale läheb vaja " + str(summa) + " meetrit lõimeniiti.")
