a2be1cbd59daaf60d5398b67ecd4ad396944f6e8
vaatanvastuhallivolvi <70329942+vaatanvastuhallivolvi@users.noreply.github.com>
2020-08-28 16:09:27 +0300

    Update peresissetulek2000.py

--- peresissetulek2000.py
+++ peresissetulek2000.py
@@ -10,11 +10,11 @@ isa_sissetulek = netopalk(netoks_isa)
 ema_sissetulek = netopalk(netoks_ema)
 lastetoetus = lapsed * 50
 
-kogu_sissetulek = round(isa_sissetulek + ema_sissetulek + lastetoetus, 2)
+fin_sissetulek = round(isa_sissetulek + ema_sissetulek + lastetoetus, 2)
 
-if str(kogu_sissetulek)[-2:] == ".0":
-    lõpp = str(kogu_sissetulek)[:-2]
+if str(fin_sissetulek)[-2:] == ".0":
+    lõpp = str(fin_sissetulek)[:-2]
 else:
-    lõpp = kogu_sissetulek
+    lõpp = fin_sissetulek
 
 print("Pere kuusissetulek on " + str(lõpp) + " krooni.")
